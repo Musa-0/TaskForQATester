1a91f317303e2822da711bf98411f98b02253956
Musa-0 <hanievmusa0504@mail.ru>
2023-08-02 12:32:44 +0300

    Fix task3

--- task3.py
+++ task3.py
@@ -3,8 +3,11 @@ param_eyes = list(map(int,input().split(" ")))
 param_eyes.sort()
 i = 0
 count = 0
-while i<cyklops-1:
-    if param_eyes[i]+1>=param_eyes[i+1]:
+while i<cyklops:
+    if i==cyklops-1:
+        count+=1
+        break
+    if param_eyes[i]+2>=param_eyes[i+1]:
         count+=1
         i+=2
     else:
